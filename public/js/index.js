eb6a00c5303ec6f569748c0c07b2423dec808f3b
Oliver Stuart <olistuart@Oli-Stuarts-macbook-pro.local>
2020-11-06 17:56:28 -0500

    Initial mobile homepage layout ready for feedback

--- public/js/index.js
+++ public/js/index.js
@@ -4,15 +4,18 @@ import { lookup } from 'dns';
 // HERO SLIDER MOBILE
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('#hero-slider', {
-        'cover': true,
-        'type': 'fade',
-        'perPage': 1,
-        'gap': 0,
-        'speed': 800,
-        'heightRatio': 1,
-        //'height': '70vh',
-        'arrows': false,
-        'autoplay': true,
-        'rewind': true
+        cover: true,
+        type: 'fade',
+        perPage: 1,
+        gap: 0,
+        speed: 800,
+        heightRatio: 1,
+        arrows: false,
+        autoplay: true,
+        rewind: true,
+        classes: {
+            pagination: 'splide__pagination home__overview-pagination', // container
+            page      : 'splide__pagination__page home__overview-page', // each button
+        }
     }).mount();
 });
